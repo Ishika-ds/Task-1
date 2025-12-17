ed78a3897fb2a3aa7981dac3f9b4e0bb01629569
Ishika-ds <ishikaambagade@gmail.com>
2025-12-17 18:08:49 +0530

    Create Personal_intro.py

---
+++ Personal_intro.py
@@ -0,0 +1,8 @@
+Name=input("What is your name?")
+Age=int(input("How old are you?"))
+Hobby=input("What is your favorite hobby?")
+City=input("Which city you live in?")
+
+print()
+print("🎉Welcome",Name,"!🎉")
+print("You are",Age,"Years old and live in",City,"and love",Hobby,".")
